eb2a47ca2b1b356972476648dccc5e31aeb398bf
Andrea <andryx1994@gmail.com>
2020-07-01 11:47:49 +0200

    Versione 0.2.5
    Implementata ricerca film, ovvero Milestone 1 completata

---
+++ js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(
+  function() {
+    //avvio funzione al click del tasto ricerca
+    $(".search").click(function(){
+      var ricercaUtente = $(".search-input").val();
+      // se input ricerca non è vuoto
+      if (ricercaUtente != ""){
+        $(".container .movie-list").text("");
+        addMovie(ricercaUtente);
+      }
+    });
+  }
+);
+
+//FUNZIONE aggiunta film
+function addMovie(movieName){
+  $.ajax(
+    {
+      url: "https://api.themoviedb.org/3/search/movie",
+      method: "GET",
+      data: {
+        api_key: "65ed57e84173ef501a3f48cc27087d06",
+        language: "it-IT",
+        query: movieName,
+      },
+      success: function (data) {
+        var risultati = data.results;
+
+        //avvio funzione printMovie ad ogni risultato della chiamata ajax
+        for (var i = 0; i < risultati.length; i++){
+          var movie = {
+            title: risultati[i].title,
+            original_title: risultati[i].original_title,
+            language: risultati[i].original_language,
+            rating: risultati[i].vote_average,
+          };
+
+          printMovie(movie);
+        }
+      },
+      error: function () {
+        alert("E' avvenuto un errore. ");
+      }
+    }
+  );
+}
+
+//FUNZIONE stampa film
+function printMovie(movie){
+  //inizializzo Handlebars con il template
+  var source = $("#movie-template").html();
+  var template = Handlebars.compile(source);
+
+  //appendo il template nel movie-list
+  var html = template(movie);
+  $(".container .movie-list").append(html);
+
+}
